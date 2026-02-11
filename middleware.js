da8b550e4168325f2c0b24c462c5de96237f166a
Shamiul40 <shamiul4040@gmail.com>
2026-02-11 23:09:57 +0600

    middleware done

--- middleware.js
+++ middleware.js
@@ -1,6 +1,39 @@
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { NextResponse } from "next/server";
 
+const locales = ["en", "bn"];
+const defaultLocale = "en";
 
-const locale = ["en", "bn"];
-const defaultLocale = "en"
+const getLocale = (request) => {
+  const acceptLanguage = request.headers.get("accept-language");
+  const headers = { "accept-language": acceptLanguage };
+  const languages = new Negotiator({ headers }).languages();
 
+  match(languages, locales, defaultLocale);
+};
 
+export async function middleware(request) {
+  const pathName = request.nextUrl.pathname;
+
+  const missingPathnameInLocale = locales.every(
+    (locale) =>
+      !pathName.startsWith(`/${locale}`) || !pathName.startsWith(`/${locale}/`),
+  );
+
+  if (missingPathnameInLocale) {
+      const locale = getLocale(request);
+
+    return NextResponse.redirect(new URL(`/${locale}/${pathName}`), request.url)
+  }
+
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    '/((?!_next).*)',
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+}
