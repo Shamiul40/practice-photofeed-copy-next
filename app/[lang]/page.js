62e62358a4b6b5def40958a046b6a8542078143a
Shamiul40 <shamiul4040@gmail.com>
2026-02-12 14:27:40 +0600

    bug fix

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { getDictionary } from "./dictionaries/getDectionary";
 
-export default async function Home({params, lang}) {
+export default async function Home({ params: { lang } }) {
 
   const dictionary = await getDictionary(lang)
   console.log(dictionary)
